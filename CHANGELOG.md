abbe3e5a5a0f78239aef9af33f705a50dab32aec
ocramz <zocca.marco@gmail.com>
2020-12-20 19:10:35 +0100

    add karate.gml

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+* 0.1.0.0 (20/12/2012)
+
+  Release candidate
+
--- src/Algebra/Graph/IO/GML.hs
+++ src/Algebra/Graph/IO/GML.hs
@@ -1,5 +1,8 @@
 {-# language OverloadedStrings #-}
 {-# options_ghc -Wno-unused-imports #-}
+-- | Mostly-complete implementation of the GML format
+--
+-- https://en.wikipedia.org/wiki/Graph_Modelling_Language
 module Algebra.Graph.IO.GML (gmlGraph, gmlGraphP, GMLGraph(..), GMLNode(..), GMLEdge(..)) where
 
 import Control.Applicative hiding (many, some)
@@ -14,32 +17,42 @@ import Text.Megaparsec (Parsec, parse, parseTest, satisfy, (<?>))
 import Text.Megaparsec.Char (space1)
 import qualified Text.Megaparsec.Char.Lexer as L
 -- parser-combinators
-import Control.Monad.Combinators (many, some, between)
+import Control.Monad.Combinators (many, some, between, skipManyTill)
 -- text
 import Data.Text (Text)
 import Data.Text.IO (readFile)
 
-import Prelude hiding (readFile)
+import Prelude hiding (readFile, takeWhile)
 
 import Algebra.Graph.IO.Internal.Megaparsec (Parser, lexeme, symbol, anyString)
 
--- | load a 'G.Graph' from a 'GMLGraph'
+-- | Construct a 'G.Graph' using the edge data contained in a 'GMLGraph'
 gmlGraph :: GMLGraph a -> G.Graph a
-gmlGraph (GMLGraph _ es) =
+gmlGraph (GMLGraph _ _ es) =
   foldl (\gr (GMLEdge a b _) -> G.edge a b `G.overlay` gr) G.empty es
 
 -- | Graph entities of the GML graph format
-data GMLGraph a = GMLGraph [GMLNode a] [GMLEdge a] deriving (Show)
+data GMLGraph a = GMLGraph {
+  gmlCreator :: Maybe String
+  , gmlNodes :: [GMLNode a]
+  , gmlEdges :: [GMLEdge a]
+  } deriving (Show)
 
 -- | Parser for the GML graph format
 gmlGraphP :: Parser a -- ^ parser for node id's
           -> Parser (GMLGraph a)
 gmlGraphP p = do
+  header <- optional creator -- header
   void $ symbol "graph"
   sqBkts $ do
     ns <- many $ gmlNode p
     es <- many $ gmlEdge p
-    pure $ GMLGraph ns es
+    pure $ GMLGraph header ns es
+
+creator :: Parser String
+creator = do
+  void $ symbol "Creator"
+  quoted $ many $ satisfy (/= '\"')
 
 gmlLabel :: Parser String
 gmlLabel = symbol "label" *> lexeme (quoted p)
