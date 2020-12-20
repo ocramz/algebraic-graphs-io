a3d48f3ea9f9098548d050bb5a8094bb8ba398fe
ocramz <zocca.marco@gmail.com>
2020-12-20 19:25:33 +0100

    add lesmiserables dataset and generalize edge parser

--- src/Algebra/Graph/IO/GML.hs
+++ src/Algebra/Graph/IO/GML.hs
@@ -27,37 +27,33 @@ import Prelude hiding (readFile, takeWhile)
 import Algebra.Graph.IO.Internal.Megaparsec (Parser, lexeme, symbol, anyString)
 
 -- | Construct a 'G.Graph' using the edge data contained in a 'GMLGraph'
-gmlGraph :: GMLGraph a -> G.Graph a
+gmlGraph :: GMLGraph a b -> G.Graph a
 gmlGraph (GMLGraph _ _ es) =
-  foldl (\gr (GMLEdge a b _) -> G.edge a b `G.overlay` gr) G.empty es
+  foldl (\gr (GMLEdge a b _ _) -> G.edge a b `G.overlay` gr) G.empty es
 
 -- | Graph entities of the GML graph format
-data GMLGraph a = GMLGraph {
-  gmlCreator :: Maybe String
+data GMLGraph a b = GMLGraph {
+  gmlHeader :: Maybe String
   , gmlNodes :: [GMLNode a]
-  , gmlEdges :: [GMLEdge a]
+  , gmlEdges :: [GMLEdge a b]
   } deriving (Show)
 
 -- | Parser for the GML graph format
 gmlGraphP :: Parser a -- ^ parser for node id's
-          -> Parser (GMLGraph a)
-gmlGraphP p = do
+          -> Parser b
+          -> Parser (GMLGraph a b)
+gmlGraphP p p2 = do
   header <- optional creator -- header
   void $ symbol "graph"
   sqBkts $ do
     ns <- many $ gmlNode p
-    es <- many $ gmlEdge p
+    es <- many $ gmlEdge p p2
     pure $ GMLGraph header ns es
 
 creator :: Parser String
 creator = do
   void $ symbol "Creator"
   quoted $ many $ satisfy (/= '\"')
-
-gmlLabel :: Parser String
-gmlLabel = symbol "label" *> lexeme (quoted p)
-  where
-    p = many $ satisfy (/= '\"')
 
 -- | GML nodes
 data GMLNode a = GMLNode a (Maybe String) deriving (Show)
@@ -76,15 +72,34 @@ quoted :: Parser a -> Parser a
 quoted = between (symbol "\"") (symbol "\"")
 
 -- | GML edges
-data GMLEdge a = GMLEdge a a (Maybe String) deriving (Show)
+data GMLEdge a b = GMLEdge a a (Maybe b) (Maybe String) deriving (Show)
 
-gmlEdge :: Parser a -> Parser (GMLEdge a)
-gmlEdge p = do
+gmlEdge :: Parser a -> Parser b -> Parser (GMLEdge a b)
+gmlEdge pa pb = do
   void $ symbol "edge"
   sqBkts $ do
-    a <- symbol "source" *> lexeme p
-    b <- symbol "target" *> lexeme p
+    a <- source pa
+    b <- target pa
+    v <- optional (value pb)
     l <- optional gmlLabel
-    pure $ GMLEdge a b l
+    pure $ GMLEdge a b v l
 
+-- attributes
+
+source, target, value :: Parser a -> Parser a
+source = attr "source"
+target = attr "target"
+value = attr "value"
+
+gmlLabel :: Parser String
+gmlLabel = symbol "label" *> lexeme (quoted p)
+  where
+    p = many $ satisfy (/= '\"')
+
+attr :: Text -> Parser a -> Parser a
+attr str p = symbol str *> lexeme p
+
+
+-- gmlValue :: Parser a -> Parser a
+-- gmlValue p = symbol "value" *> lexeme p
 
