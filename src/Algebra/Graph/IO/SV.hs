fac24a24797d0f371e54d47e066bcd5bf0954f05
ocramz <zocca.marco@gmail.com>
2020-12-21 12:31:53 +0100

    add TSV support via csv-conduit

---
+++ src/Algebra/Graph/IO/SV.hs
@@ -0,0 +1,22 @@
+{-# language OverloadedStrings #-}
+{-# options_ghc -Wno-unused-imports #-}
+module Algebra.Graph.IO.SV where
+
+import Data.CSV.Conduit (CSV(..), CSVSettings(..))
+
+-- | tab-separated values
+tsvSettings :: CSVSettings
+tsvSettings = CSVSettings '\t' Nothing
+
+
+{-
+myProcessor :: Conduit (MapRow Text) m (MapRow Text)
+myProcessor = undefined
+
+test = runResourceT $ runConduit $
+  sourceFile "test/BigFile.csv" .|
+  intoCSV defCSVSettings .|
+  myProcessor .|
+  (writeHeaders defCSVSettings >> fromCSV defCSVSettings) .|
+  sinkFile "test/BigFileOut.csv"
+-}
